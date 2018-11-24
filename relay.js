a607c4c7fee0571d24d289b5a35a6855f1fea6bd
Oelin <42823429+Oelin@users.noreply.github.com>
2018-11-24 02:30:45 -0800

    Update relay.js

--- relay.js
+++ relay.js
@@ -1,4 +1,5 @@
 var net = require('net');
+
 var io = require('socket.io')(80);
 
 
@@ -8,18 +9,18 @@ io.on('connection', (socket) => {
 
     socket.on('create', (address) => {
         remote = net.createConnection(address);
-
+    
         remote.on('data', (buffer) => {
-            var text = buffer.toString('utf-8');
+            let text = buffer.toString('utf-8');
             
             socket.emit('reply', text);
         });
     });
-
-    socket.on('relay', (text) => {
+    
+    socket.on('send', (text) => {
         remote.write(text);
     });
-
+    
     socket.on('end', () => {
         remote.end();
     });
