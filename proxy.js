89c11f8c663e5936076a5c776c34822c3488f1ee
Oelin <42823429+Oelin@users.noreply.github.com>
2020-06-25 11:26:06 +0100

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,31 +1,30 @@
-var io = require('socket.io')(80);
-var net = require('net');
+var io = require('socket.io')(80)
+var net = require('net')
 
 
 
 io.on('connection', function(socket) 
 {
-    var remote;
+    let remote
     
     socket.on('create', function(address) 
     {
-        remote = net.createConnection(address);
+        remote = net.createConnection(address)
     
         remote.on('data', function(buffer) 
         {
-            var data = buffer.toString();
-            
-            socket.emit('reply', data);
-        });
-    });
+            let data = buffer.toString()
+            socket.emit('reply', data)
+        })
+    })
     
     socket.on('send', function(data) 
     {
-        remote.write(data);
-    });    
+        remote.write(data)
+    });   
    
     socket.on('end', function()
     {
-        remote.end();
-    });
-});
+        remote.end()
+    })
+})
